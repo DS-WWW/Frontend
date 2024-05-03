977e986b62ae7ac38eeffcb7f79a49fc101d4481
홍수연 <htndus1309@gmail.com>
2024-05-04 01:23:31 +0900

    navbar, footer 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import '../css/Footer.css';
 
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <div className='footer'>
+      <div className="footer-content">COPYRIGHT © WWW, ALL RIGHTS RESERVED.</div>
+      <div>김은서 정해인 양지선 홍수연</div>
+    </div>
   )
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,31 +2,44 @@ import React from 'react'
 import { Outlet, NavLink } from 'react-router-dom';
 import Footer from './Footer';
 import "../css/Navbar.css";
+import logo from "../images/logo.png"
 
 const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        <div className='left'>
-          <NavLink to='/' className='navlink'>
-            Navbar
-          </NavLink>
-          <NavLink to='/Catfeed' className='navlink'>
-            Catfeed
-          </NavLink>
-          <NavLink to='/Hospital' className='navlink'>
-            Hospital
-          </NavLink>
-          <NavLink to='/Catfeed/CatfeedDetail' className='navlink'>
-            CatfeedDetail
-          </NavLink>
-          <NavLink to='/Catcamera' className='navlink'>
-            Catcamera
-          </NavLink>
-          <NavLink to='/Cardnews' className='navlink'>
-            Cardnews
+        <div className='nav-left'>
+          <NavLink to='/' className="nav-logo">
+            <img alt="홈 아이콘" src={logo} height="60px" />
           </NavLink>
         </div>
+        <ul className='nav-right'>
+          <li className='nav-li'>
+            <NavLink to='/Catfeed' className='nav-link'>
+              Catfeed
+            </NavLink>
+          </li>
+          <li className='nav-li'>
+            <NavLink to='/Hospital' className='nav-link'>
+              Hospital
+            </NavLink>
+          </li>
+          <li className='nav-li'>
+            <NavLink to='/Catfeed/CatfeedDetail' className='nav-link'>
+              CatfeedDetail
+            </NavLink>
+          </li>
+          <li className='nav-li'>
+            <NavLink to='/Catcamera' className='nav-link'>
+              Catcamera
+            </NavLink>
+          </li>
+          <li className='nav-li'>
+            <NavLink to='/Cardnews' className='nav-link'>
+              Cardnews
+            </NavLink>
+          </li>
+        </ul>
       </div>
       <div>
           <Outlet />
