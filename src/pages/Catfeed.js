e3c47e75997d817642683a4a8abc18e709070ea1
홍수연 <htndus1309@gmail.com>
2024-05-13 17:01:43 +0900

    지도 페이지들

--- src/pages/Catfeed.js
+++ src/pages/Catfeed.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import CatfeedList from '../components/CatfeedList';
+import KakaoMap from '../components/KakaoMap';
+import "../css/Catfeed.css"
 
 const Catfeed = () => {
+
+  const today = new Date()
+  const formattedYear = today.getFullYear().toString().slice(-2);
+  const formattedDate = `${formattedYear}.${today.getMonth() + 1}.${today.getDate()} ${today.getHours()}시 기준`
+
   return (
-    <div>급식소 지도 예정</div>
+    <>
+      <div className='catfeed-map'>
+        <text className='title'>고양이 급식소 현황</text>
+        <div className='time'>{formattedDate}</div>
+      </div>
+      <KakaoMap />
+      <div>
+        <div className='catfeed-list'>
+          <text className='title'>급식소 목록</text>
+        </div>
+        <CatfeedList />
+        <CatfeedList />
+        <CatfeedList />
+        <CatfeedList />
+      </div>
+    </>
   );
 }
 
